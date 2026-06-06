55f51c505b9578fee71391581489212a92759968
Chris Trombly <christrombly001@hotmail.com>
2026-06-06 12:17:47 -0400

    MariaSQL functioning in phpMyAdmin

---
+++ contract_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE contract_data (
+  contract_ID int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  sign_date VARCHAR(255) NOT NULL,
+  witness VARCHAR(255) NOT NULL
+) ENGINE = InnoDB;
