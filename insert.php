b7db497ebd4b2520d013ac312150f1034bff757c
Chris Trombly <christrombly001@hotmail.com>
2026-06-11 13:31:50 -0400

    Inserted first row of data using PHP

--- insert.php
+++ insert.php
@@ -8,5 +8,27 @@ $password = 'Squid';
 // PDO Object Setup
 $db = new PDO($dsn, $username, $password);
 
+// PHP Variables
+$insert_id = 2;
+$name = 'SquidBoy';
+$date = 'Go-Kart Glory Days';
+$witness = 'The Demon';
+
+
+$query = 'INSERT INTO contract_data
+				(contract_ID, name, sign_date, witness)
+			 VALUES
+			 	(:insert_id, :name, :date, :witness)';
+
+$statement = $db->prepare($query);
+$statement->bindValue(':insert_id', $insert_id);
+$statement->bindValue(':name', $name);
+$statement->bindValue('date', $date);
+$statement->bindValue('witness', $witness);
+
+$statement->execute();
+$statement->closeCursor();
+
+
 
 ?>
